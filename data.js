b96351de6bc95551d7eae94bd595879f9bfa9990
Wei Gao <wgao19@gmail.com>
2019-03-23 16:51:50 +0800

    Add talk site

---
+++ data.js
@@ -0,0 +1,58 @@
+var talks = [
+  {
+    title: "Trip to React Rally",
+    when: "September 9, 2018",
+    where: {
+      name: "React Knowledgeable",
+      link: "https://www.facebook.com/reactknowledgeable/"
+    },
+    post:
+      "https://github.com/wgao19/sharing-talks/blob/master/sharings/2018_9_9-react_rally/README.md",
+    intro:
+      "I went to React Rally 2018. This is the origins of all sins happening here."
+  },
+  {
+    title: "Journey to Our Future Sharing",
+    when: "September 21, 2018",
+    where: {
+      name: "React Knowledgeable",
+      link: "https://www.facebook.com/reactknowledgeable/"
+    },
+    post:
+      "https://github.com/wgao19/sharing-talks/blob/master/sharings/2018_9_21-journey-to-our-future-sharing-1/README.md",
+    slides: "https://slides.com/wgao19/future-sharing-1#/",
+    intro:
+      "My pitch to start organizing our internal sharing as React Knowledgeable"
+  },
+  {
+    title: "Makes of a Good Talk",
+    when: "January 25, 2019",
+    where: {
+      name: "React Knowledgeable",
+      link: "https://www.facebook.com/reactknowledgeable/"
+    },
+    slides: "https://slides.com/wgao19/what-makes-a-good-sharing-talk",
+    intro:
+      "Sharing our understanding about what makes good talks at React Knowledgeable, and how to prepare for them"
+  },
+  {
+    title: "What's Happening at 30 Days of CSS Girls",
+    when: "hopefully soon",
+    where: {
+      name: "Talk.CSS",
+      link: "https://singaporecss.github.io"
+    },
+    // slides: "slides/2019-03--whats-happening-30-days-css-girls",
+    intro: `A retro talk regarding the <a href="http://css.codinggirls.sg">#30DaysOfCSSGirls</a> event with <a href="http://codinggirls.sg">CodingGirls</a>`
+  },
+  {
+    title: "This World Mixed and Blended",
+    when: "April 24, 2019",
+    where: {
+      name: "Talk.CSS",
+      link: "https://singaporecss.github.io"
+    },
+    intro:
+      "Introducing `mix-blend-mode` and showcase designs implemented with it"
+  }
+];
