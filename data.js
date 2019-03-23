8e4e6caba6b56100ff128c346cf4d16a01ad7621
Wei Gao <wgao19@gmail.com>
2019-03-23 17:32:48 +0800

    Separate upcoming and past talks and update styling

--- data.js
+++ data.js
@@ -1,39 +1,13 @@
 var talks = [
   {
-    title: "Trip to React Rally",
-    when: "September 9, 2018",
+    title: "This World Mixed and Blended",
+    when: "04/24/2019",
     where: {
-      name: "React Knowledgeable",
-      link: "https://www.facebook.com/reactknowledgeable/"
+      name: "Talk.CSS",
+      link: "https://singaporecss.github.io"
     },
-    post:
-      "https://github.com/wgao19/sharing-talks/blob/master/sharings/2018_9_9-react_rally/README.md",
     intro:
-      "I went to React Rally 2018. This is the origins of all sins happening here."
-  },
-  {
-    title: "Journey to Our Future Sharing",
-    when: "September 21, 2018",
-    where: {
-      name: "React Knowledgeable",
-      link: "https://www.facebook.com/reactknowledgeable/"
-    },
-    post:
-      "https://github.com/wgao19/sharing-talks/blob/master/sharings/2018_9_21-journey-to-our-future-sharing-1/README.md",
-    slides: "https://slides.com/wgao19/future-sharing-1#/",
-    intro:
-      "My pitch to start organizing our internal sharing as React Knowledgeable"
-  },
-  {
-    title: "Makes of a Good Talk",
-    when: "January 25, 2019",
-    where: {
-      name: "React Knowledgeable",
-      link: "https://www.facebook.com/reactknowledgeable/"
-    },
-    slides: "https://slides.com/wgao19/what-makes-a-good-sharing-talk",
-    intro:
-      "Sharing our understanding about what makes good talks at React Knowledgeable, and how to prepare for them"
+      "Introducing `mix-blend-mode` and showcase designs implemented with it."
   },
   {
     title: "What's Happening at 30 Days of CSS Girls",
@@ -43,16 +17,45 @@ var talks = [
       link: "https://singaporecss.github.io"
     },
     // slides: "slides/2019-03--whats-happening-30-days-css-girls",
-    intro: `A retro talk regarding the <a href="http://css.codinggirls.sg">#30DaysOfCSSGirls</a> event with <a href="http://codinggirls.sg">CodingGirls</a>`
+    intro: `A retro talk regarding the <a href="http://css.codinggirls.sg">#30DaysOfCSSGirls</a> event with <a href="http://codinggirls.sg">CodingGirls</a>.`
   },
+
   {
-    title: "This World Mixed and Blended",
-    when: "April 24, 2019",
+    title: "Makes of a Good Talk",
+    when: "01/25/2019",
     where: {
-      name: "Talk.CSS",
-      link: "https://singaporecss.github.io"
+      name: "React Knowledgeable",
+      link: "https://www.facebook.com/reactknowledgeable/"
     },
+    slides: "https://slides.com/wgao19/what-makes-a-good-sharing-talk",
     intro:
-      "Introducing `mix-blend-mode` and showcase designs implemented with it"
+      "Sharing our understanding about what makes good talks at React Knowledgeable, and how to prepare for them."
+  },
+
+  {
+    title: "Journey to Our Future Sharing",
+    when: "09/21/2018",
+    where: {
+      name: "React Knowledgeable",
+      link: "https://www.facebook.com/reactknowledgeable/"
+    },
+    post:
+      "https://github.com/wgao19/sharing-talks/blob/master/sharings/2018_9_21-journey-to-our-future-sharing-1/README.md",
+    slides: "https://slides.com/wgao19/future-sharing-1#/",
+    intro:
+      "My pitch to start organizing our internal sharing as React Knowledgeable."
+  },
+
+  {
+    title: "Trip to React Rally",
+    when: "09/09/2018",
+    where: {
+      name: "React Knowledgeable",
+      link: "https://www.facebook.com/reactknowledgeable/"
+    },
+    post:
+      "https://github.com/wgao19/sharing-talks/blob/master/sharings/2018_9_9-react_rally/README.md",
+    intro:
+      "I went to React Rally 2018. This is the origins of all sins happening here."
   }
 ];
